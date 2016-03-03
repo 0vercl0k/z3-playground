fa991a3acedf49ac5309f54787b96366df4709c6
Axel 0vercl0k Souchet <0vercl0k@tuxfamily.org>
2016-03-02 20:00:55 -0800

    polished the script a little bit

--- graph_optimization_z3.py
+++ graph_optimization_z3.py
@@ -52,16 +52,16 @@ def min_dom_set(graph):
     s = Optimize()
     nodes_colors = dict((node_name, Int('k%r' % node_name)) for node_name in graph.nodes())
     for node in graph.nodes():
-           s.add(And(nodes_colors[node] >= 0, nodes_colors[node] <= 1))
+           s.add(And(nodes_colors[node] >= 0, nodes_colors[node] <= 1)) # dominator or not
            dom_neighbor = Sum ([ (nodes_colors[j]) for j in graph.neighbors(node) ])
-           s.add(Sum(nodes_colors[node], dom_neighbor ) >=1)
-    s.minimize( Sum([ nodes_colors[y] for y in graph.nodes()]) )
+           s.add(Sum(nodes_colors[node], dom_neighbor ) >= 1 )
+    s.minimize( Sum([ nodes_colors[y] for y in graph.nodes() ]) )
 
-    if s.check() == unsat:
-        raise Exception('Could not find a solution.')
-    else:
+    if s.check() == sat:
         m = s.model()
         return dict((name, m[color].as_long()) for name, color in nodes_colors.iteritems())
+    
+    raise Exception('Could not find a solution.')
 
 def build_peternson_3_coloring_graph():
     """Build http://en.wikipedia.org/wiki/File:Petersen_graph_3-coloring.svg"""
@@ -97,6 +97,13 @@ def main(argc, argv):
         build_fat_graph()
     ]
 
+    color_available = [
+        'red',
+        'blue',
+        'green',
+        'pink'
+    ]
+
     for G, name, layout in Gs:
         print 'Trying to color %s now (%d nodes, %d edges)..' % (repr(name), G.number_of_nodes(), G.number_of_edges())
         t1 = time.time()
@@ -109,14 +116,6 @@ def main(argc, argv):
         else:
             print 'Too long, see the .png!'
 
-        print 'Setting the colors on the nodes..'
-        color_available = [
-            'red',
-            'blue',
-            'green',
-            'pink'
-        ]
-
         for node in G.nodes_iter():
             n = G.get_node(node)
             n.attr['color'] = color_available[s[node]]
@@ -127,32 +126,24 @@ def main(argc, argv):
         print '---'
 
     for G, name, layout in Gs:
-        print 'Trying find min dom set for %s now (%d nodes, %d edges)..' % (repr(name), G.number_of_nodes(), G.number_of_edges())
+        print 'Trying to find min dom set for %s now (%d nodes, %d edges)..' % (repr(name), G.number_of_nodes(), G.number_of_edges())
         t1 = time.time()
         s = min_dom_set(G)
         t2 = time.time()
-        print 'OK, found a solution with %d dominators' % sum( s.values())
-        print 'Here is the solution (in %ds):' % (t2 - t1)
+
+        print 'OK, found a solution with %d dominators (in %ds), here it is:' % (sum(s.values()), (t2 - t1))
         if len(s) < 20:
             print s
         else:
             print 'Too long, see the .png!'
 
-        print 'Setting the dominators to blue..'
-        color_available = [
-            'red',
-            'blue',
-            'green',
-            'pink'
-        ]
-
         for node in G.nodes_iter():
             n = G.get_node(node)
             n.attr['color'] = color_available[s[node]]
 
-        print 'Saving it in the current directory with the layout %s..' % repr(layout)
+        print 'Saving it in the current directory with the layout %s (dominators in blue)..' % repr(layout)
         G.layout(layout)
-        G.draw('./graph_z3_%s_dominatied.png' % name)
+        G.draw('./graph_z3_%s_dominated.png' % name)
         print '---'
 
     return 1
